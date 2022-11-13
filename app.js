ad4d269149ad0dfc20b0582c1d85adb16e07290e
Jeevan <jboyini@kpininja.com>
2022-11-13 22:36:14 +0530

    methods concept

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const app = Vue.createApp({
             finishCourse: 'Need to Finish Vue Course',
             aboutVueLink: 'https://vuejs.org/'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randNum = Math.random();
+            if (randNum < 0.5)
+                return 'Learn Vue!'
+            else
+                return 'Master Vue!'
+        }
     }
 });
 app.mount('#user-goal'); // as we are using the id:user-goal from the HTML <section> ele we have full contrl on this
